ea32c27677cf333fcd62273c0c32d2fa3a74e302
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-04-24 14:21:14 +0200

    add: class familier and get & set

---
+++ models/Familier.php
@@ -0,0 +1,152 @@
+<?php
+
+class Familier
+{
+  private $_idFamilier;
+  private $_nomFamilier;
+  private $_vieMaxFamilier;
+  private $_vieFamilier;
+  private $_forceFamilier;
+  private $_niveauFamilier;
+  private $_photoFamilier;
+
+  /**
+   * Get the value of _idFamilier
+   */
+  public function get_idFamilier()
+  {
+    return $this->_idFamilier;
+  }
+
+  /**
+   * Set the value of _idFamilier
+   *
+   * @return  self
+   */
+  public function set_idFamilier($_idFamilier)
+  {
+    $this->_idFamilier = $_idFamilier;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of _nomFamilier
+   */
+  public function get_nomFamilier()
+  {
+    return $this->_nomFamilier;
+  }
+
+  /**
+   * Set the value of _nomFamilier
+   *
+   * @return  self
+   */
+  public function set_nomFamilier($_nomFamilier)
+  {
+    $this->_nomFamilier = $_nomFamilier;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of _vieMaxFamilier
+   */
+  public function get_vieMaxFamilier()
+  {
+    return $this->_vieMaxFamilier;
+  }
+
+  /**
+   * Set the value of _vieMaxFamilier
+   *
+   * @return  self
+   */
+  public function set_vieMaxFamilier($_vieMaxFamilier)
+  {
+    $this->_vieMaxFamilier = $_vieMaxFamilier;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of _vieFamilier
+   */
+  public function get_vieFamilier()
+  {
+    return $this->_vieFamilier;
+  }
+
+  /**
+   * Set the value of _vieFamilier
+   *
+   * @return  self
+   */
+  public function set_vieFamilier($_vieFamilier)
+  {
+    $this->_vieFamilier = $_vieFamilier;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of _forceFamilier
+   */
+  public function get_forceFamilier()
+  {
+    return $this->_forceFamilier;
+  }
+
+  /**
+   * Set the value of _forceFamilier
+   *
+   * @return  self
+   */
+  public function set_forceFamilier($_forceFamilier)
+  {
+    $this->_forceFamilier = $_forceFamilier;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of _niveauFamilier
+   */
+  public function get_niveauFamilier()
+  {
+    return $this->_niveauFamilier;
+  }
+
+  /**
+   * Set the value of _niveauFamilier
+   *
+   * @return  self
+   */
+  public function set_niveauFamilier($_niveauFamilier)
+  {
+    $this->_niveauFamilier = $_niveauFamilier;
+
+    return $this;
+  }
+
+  /**
+   * Get the value of _photoFamilier
+   */
+  public function get_photoFamilier()
+  {
+    return $this->_photoFamilier;
+  }
+
+  /**
+   * Set the value of _photoFamilier
+   *
+   * @return  self
+   */
+  public function set_photoFamilier($_photoFamilier)
+  {
+    $this->_photoFamilier = $_photoFamilier;
+
+    return $this;
+  }
+}
