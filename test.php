8af976192802eb19116f117015603bf8c25846e3
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-04-24 15:18:55 +0200

    add: session_start & destroy w/ log out button

--- test.php
+++ test.php
@@ -1,5 +1,18 @@
 <?php
+session_start();
+$_SESSION['nom'] = null;
+session_destroy();
+header('Location: redirection tu connais');
+?>
 
 
-
-
+<?php
+session_start();
+if (isset($_SESSION['nom'])) {
+  echo 'BOUTON';
+} else {
+  setcookie("TestCookie", $_SESSION['nom'], time() + 1800);
+?>
+  <a href="controller/logout.php" type="button" class="btn btn-primary btn-sm">Log out</a>
+  <span><?php echo $_SESSION['nom']; ?></span>
+<?php } ?>
