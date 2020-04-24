2dac8d76a71e43ca7a3e37dcb49323197d348e1c
Hermoult <adrienhergat@gmail.com>
2020-04-24 16:31:11 +0200

    familier

--- fiche_personnage/familier.php
+++ fiche_personnage/familier.php
@@ -6,4 +6,22 @@ require('../models/Familier.php');
 $a = new FamilierManager;
 $familier = $a->readAll();
 
-var_dump($familier['0']);
+// count pour savoir le nbre de familier
+// foreach pour passer les tableaux
+
+
+$b = new Familier;
+$familier1 = $b->hydrate($familier['0']);
+$vie1 = $b->get_vie_familier();
+$nom1 = $b->get_nom_familier();
+$force1 = $b->get_force_familier();
+
+
+echo $familier1 . $vie1 . $nom1 . $force1;
+
+
+
+$familier2 = $b->hydrate($familier['1']);
+$familier3 = $b->hydrate($familier['2']);
+$familier4 = $b->hydrate($familier['3']);
+$familier5 = $b->hydrate($familier['4']);
