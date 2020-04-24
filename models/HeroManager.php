4d9941bda8fa3d4f156eaad1d7860efde5d48cfa
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-04-24 15:33:00 +0200

    change bdd name

--- models/HeroManager.php
+++ models/HeroManager.php
@@ -1,22 +1,24 @@
-<?php   
+<?php
 //'user' à modifier en remplaçant par le nom de la table (ctrl+d pour modifier plusieurs, Alt pour déselectionner)
 
-class heroManager  {
+class heroManager
+{
 
     private $pdo;
 
-    const ADDRESS='mysql:host=localhost;dbname=hackaton2'; //Valeur du dbname à modifier en fonction du nom de la base de donnée
-    const USER='adrien';
-    const PASS='adrien';
+    const ADDRESS = 'mysql:host=localhost;dbname=hackaton2'; //Valeur du dbname à modifier en fonction du nom de la base de donnée
+    const USER = 'root';
+    const PASS = '';
 
-    public function __construct ()
+    public function __construct()
     {
         try {
-            $this->pdo = new PDO(self::ADDRESS,self::USER,self::PASS,array(PDO::ATTR_PERSISTENT => true));
+            $this->pdo = new PDO(self::ADDRESS, self::USER, self::PASS, array(PDO::ATTR_PERSISTENT => true));
         } catch (\Throwable $th) {
-            die('error sql connection'.$th);
+            die('error sql connection' . $th);
         }
     }
+
 
     public function readAll()
     {
@@ -26,11 +28,11 @@ class heroManager  {
             $contact = $pdoStatement->fetchAll();
             return $contact;
         } catch (\Throwable $th) {
-            die('error readAll connection'.$th);
+            die('error readAll connection' . $th);
         }
     }
 
-    public function read($pseudo) 
+    public function read($pseudo)
     {
         try {
             $pdoStatement = $this->pdo->prepare('SELECT * FROM heros WHERE nom_heros = ?');
@@ -38,29 +40,29 @@ class heroManager  {
             $contact = $pdoStatement->fetch();
             return $contact;
         } catch (\Throwable $th) {
-            die('error read connection'.$th);
+            die('error read connection' . $th);
         }
     }
 
-    public function create($pseudo,$pass,$date) // parametre à renseigner => nom des champs (ctrl+d pour modifier dans execute)
+    public function create($pseudo, $pass, $date) // parametre à renseigner => nom des champs (ctrl+d pour modifier dans execute)
     {
         try {
             $pdoStatement = $this->pdo->prepare('INSERT INTO hero(pseudo,pass,d) VALUES (?,?,?)');
-            $pdoStatement->execute([$pseudo,$pass,$date]);
+            $pdoStatement->execute([$pseudo, $pass, $date]);
             echo 'profil créé';
         } catch (\Throwable $th) {
-            die('error create connection'.$th);
+            die('error create connection' . $th);
         }
     }
 
-    public function update($pseudo,$pass,$d) // parametre à renseigner => nom des champs (ctrl+d pour modifier dans execute)
+    public function update($pseudo, $pass, $d) // parametre à renseigner => nom des champs (ctrl+d pour modifier dans execute)
     {
         try {
             $pdoStatement = $this->pdo->prepare('UPDATE hero SET pass=?,d=? WHERE pseudo = ?');
-            $pdoStatement->execute([$pass,$d,$pseudo]);
+            $pdoStatement->execute([$pass, $d, $pseudo]);
             echo 'profil mis à jour';
         } catch (\Throwable $th) {
-            die('error uptade connection'.$th);
+            die('error uptade connection' . $th);
         }
     }
 }
